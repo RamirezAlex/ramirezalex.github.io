910904d7d9e96fcb87910495a0aad4c7231949a2
Alex Ramirez <alexander.ramirez@gmail.com>
2024-03-10 08:39:52 -0500

    Add Google Analytics

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -1,9 +1,17 @@
 use leptos::*;
+use leptos_meta::Script;
 
 #[component]
 pub fn Footer() -> impl IntoView {
     view! {
         <footer>
+            <Script async_="true" src="https://www.googletagmanager.com/gtag/js?id=G-LMD9JFSZTB"></Script>
+            <Script>{r#"
+                window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-LMD9JFSZTB');
+            "#}</Script>
             RamirezAlex - 2024
         </footer>
     }
